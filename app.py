b61ce558ac99f149895c28ad245977148d1907b7
Zar Hlyan Aung <127838777+ZarHlyanAung@users.noreply.github.com>
2023-04-17 08:34:54 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,32 @@
-# import Libraries
 import streamlit as st
-import tensorflow as tf
-import cv2
 from PIL import Image
 import numpy as np
-# import matplotlib.pyplot as plt
-# import imutils
-# from imutils.perspective import four_point_transform
-# from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input as mobilenet_v2_preprocess_input
 
-
-# <---- functions start ---->
-
-# loaded the model from other path
-@st.cache_data
-def loadModel():
-    model = tf.keras.models.load_model(
-        "version-nine-from-five.h5", compile=False)
-    return model
-
-
-model = loadModel()
-
-
-# <--- factions end --->
-
-
-st.markdown("<h3 style='color: #1A5F7A;'>Dog or Cat classification</h3>",
-            unsafe_allow_html=True)
-st.write("* no two animals in one pic, only one cat or dog input image is for good prediction")
-
-
-# load file
-uploaded_file = st.file_uploader("Upload Your Image", type=["png", "jpg"])
-
-if uploaded_file is None:
-    st.success("Please upload your image")
-
-elif uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Image Input')
-
-    # Resize the image to 100x100
-    resized = cv2.resize(np.array(image), (100, 100))
-    resized = mobilenet_v2_preprocess_input(resized)
-    img_reshape = resized[np.newaxis, ...]
-
-    genrate = st.button('Genrate Prediction')
-    if genrate:
-        prediction = model.predict(img_reshape)
-        st.success(prediction)
-        st.success(type(prediction[0]))
-        if prediction[0] >= 0.5:
-            st.header("Dog")
-        else:
-            st.header("Cat")
+# Define the Streamlit app
+def app():
+    # Set the title and sidebar
+    st.title('Cat vs Dog Classifier')
+    st.sidebar.title('Upload Image')
+    
+    # Create a file uploader in the sidebar
+    uploaded_file = st.sidebar.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
+    
+    if uploaded_file is not None:
+        # Read the uploaded image
+        image = Image.open(uploaded_file)
+        
+        # Display the uploaded image
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+        # Preprocess the image for model input
+        image = image.resize((64, 64)) # Resize the image to match the input size of the model
+        image = np.array(image) # Convert the image to a numpy array
+        image = image / 255.0 # Normalize the pixel values to [0, 1]
+        image = np.expand_dims(image, axis=0) # Add an extra dimension for batch size
+        
+        # Make predictions with the model
+        prediction = model.predict(image)
+        predicted_label = 'cat' if prediction[0][0] > 0.5 else 'dog' # Assuming threshold of 0.5 for binary classification
+        
+        # Display the predicted label
+        st.write('Predicted Label:', predicted_label)
